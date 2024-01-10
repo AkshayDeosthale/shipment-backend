d23c1d2f827fe28e6b4137468d70666d002e53b8
AkshayDeosthale <akshay.deosthale6@gmail.com>
2024-01-10 13:17:36 +0530

    [FIX] Vehicle Socket

--- src/vehicle-location/dto/update-vehicle-location.dto.ts
+++ src/vehicle-location/dto/update-vehicle-location.dto.ts
@@ -1,6 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateVehicleLocationDto } from './create-vehicle-location.dto';
 
-export class UpdateVehicleLocationDto extends PartialType(CreateVehicleLocationDto) {
+export class UpdateVehicleLocationDto extends PartialType(
+  CreateVehicleLocationDto,
+) {
   id: number;
 }
